82b5b5b7427def92b7a0ade64b4d36ee25761e73
Marcus Willock <crazcalm@gmail.com>
2020-04-30 19:28:20 -0400

    Amazon find slots mvp is done

--- sites/amazon/scripts/find_amazon_slots.py
+++ sites/amazon/scripts/find_amazon_slots.py
@@ -8,15 +8,16 @@ from libs.base import get_webdriver
 from sites.amazon.page import Amazon
 
 
-DESCRIPTION = "log into amazon and close the browser"
+DESCRIPTION = "Logs into the Amazon site and refreshes the Amazon fresh checkout page until it finds an open slot"
 DRIVER_HELP = "Selects which web browser to use"
 EMAIL_HELP = "Amazon email"
 PASSWORD_HELP = "Amazon email"
 REFRESH_RATE = "Page refresh rate per seconds"
 
+DEFAULT_SOUND_FILE = path.join("sound_files", "open_slot.mp3")
+
 
 def main():
-    # TODO: Add Browser choice and set default browser to firefox
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument(
         "--driver",
@@ -31,49 +32,47 @@ def main():
     parser.add_argument(
         "--refresh", "-r", type=int, default=40, help=REFRESH_RATE, dest="refresh"
     )
+    parser.add_argument(
+        "--sound_file",
+        "-s",
+        type=str,
+        default=DEFAULT_SOUND_FILE,
+        dest="sound_file_path",
+    )
 
     args = parser.parse_args()
 
-    driver_name = "firefox"
-
-    driver = get_webdriver(driver_name=driver_name)
+    driver = get_webdriver(driver_name=args.driver)
     browser = Amazon(driver=driver, implicit_wait=10)
     browser.get_homepage()
-
     browser.login(email=args.email, password=args.password)
 
-    # Experimental
     browser.click_checkout_cart()
-
     browser.click_amazon_fresh_checkout_button()
-
     browser.click_on_proceed_to_checkout_button()
 
-    sleep(5)
+    # TODO: Figure out how to properly wait...
+    sleep(3)
 
     any_slots = browser.any_available_slots()
     while not any_slots:
 
-        # TODO -- Add refresh rate to cmd args
+        # Sleep until in need to refresh the page
         sleep(args.refresh)
 
         browser.page_refresh()
 
-        # TODO: look into waiting properly
-        sleep(4)
+        # TODO: Figure out how to properly wait...
+        sleep(3)
 
         # Need to wait till slots has fully rendered
         any_slots = browser.any_available_slots()
 
         # TODO: consider adding verbose output
-        print(any_slots)
+        # TODO: consider adding a timestamp
+        print(f"any open slots: {any_slots}")
 
-    # TODO: add the sound file to cmd args
-    sound_file_path = path.join("sound_files", "open_slot.mp3")
-    playsound(sound_file_path)
-
-    # TODO: remove before committing to Master
-    browser.quit()
+    playsound(args.sound_file_path)
 
 
 if __name__ == "__main__":
